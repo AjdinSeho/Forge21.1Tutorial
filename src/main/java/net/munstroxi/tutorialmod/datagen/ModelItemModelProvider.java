871cc0c73b0a962c793754f5e9455a8ecad3b17c
Ajdin Seho <ajdinseho@hotmail.com>
2024-12-19 10:42:26 -0500

    Added datagen

--- src/main/java/net/munstroxi/tutorialmod/datagen/ModelItemModelProvider.java
+++ src/main/java/net/munstroxi/tutorialmod/datagen/ModelItemModelProvider.java
@@ -4,6 +4,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.munstroxi.tutorialmod.TutorialMod;
+import net.munstroxi.tutorialmod.item.ModItems;
 
 public class ModelItemModelProvider extends ItemModelProvider {
 
@@ -13,10 +14,11 @@ public class ModelItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        basicItem(Mode)
+        basicItem(ModItems.ALEXANDRITE.get());
+        basicItem(ModItems.RAW_ALEXANDRITE.get());
+        basicItem(ModItems.CHISEL.get());
+        basicItem(ModItems.KOHLRABI.get());
+        basicItem(ModItems.AURORA_ASHES.get());
+        basicItem(ModItems.DIEGO.get());
     }
-
-
-
-
 }
